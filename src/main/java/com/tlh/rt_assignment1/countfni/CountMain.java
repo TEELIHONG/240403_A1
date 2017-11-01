524e6619b3b8e409fb81d1b5d2976f6bdcc27425
TEE LI HONG <31922731+TEELIHONG@users.noreply.github.com>
2017-11-01 19:51:39 +0800

    STIW3054-A171 Assignment#1 240403-TeeLiHong

--- src/main/java/com/tlh/rt_assignment1/countfni/CountMain.java
+++ src/main/java/com/tlh/rt_assignment1/countfni/CountMain.java
@@ -4,7 +4,6 @@
 //Task: #Assignment1
 //Matrik: #240403
 //Name: #Tee Li Hong
-
 package com.tlh.rt_assignment1.countfni;
 
 import java.io.File;
@@ -12,20 +11,19 @@ import java.util.ArrayList;
 
 public class CountMain {
 
-    public static final String PATH ="Test_files";
-    
-    public static void main(String[] args) throws Exception{
-        
+    public static final String PATH = "Test_files";
+
+    public static void main(String[] args) throws Exception {
+
         //Class Path
         Path pathName = new Path(PATH);
         Thread pth = new Thread(pathName);
         pth.start();
         pth.join();
         String pfolder = Path.getPath();
-        
-       //Check path
+
+        //Check path
         //System.out.println("my path is " + pfolder);
-        
         //Class FileList
         final File dir = new File(pfolder);
         FileList fList = new FileList(dir);
@@ -33,7 +31,7 @@ public class CountMain {
         fl.start();
         fl.join();
         ArrayList<String> fileL = FileList.getList();
-        
+
         //Class FileIssue
         FileIssue cfile = new FileIssue(fileL, pfolder);
         Thread cf = new Thread(cfile);
@@ -41,11 +39,10 @@ public class CountMain {
         cf.join();
         int countf = FileIssue.getCountFile();
         int counti = FileIssue.getCountIssue();
-        
+
         //Output
         System.out.println("Number of Java Files = " + countf);
         System.out.println("Number of Issues = " + counti);
     }
-    
-    
+
 }
